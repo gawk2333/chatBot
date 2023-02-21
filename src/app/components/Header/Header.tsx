e70cf0a5f0e357e3627a4f0b8624f5995ef0164d
Kang Gao <kang.g1988@gmail.com>
2023-02-21 21:30:32 +1300

    Add setting button

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { Icon, Popup } from "semantic-ui-react";
 import styles from "./header.module.css";
 
 export default function Header() {
   return (
     <div className={styles.header}>
       <div className={styles.logo}>Chat bot</div>
+      <div className={styles.setting}>
+        <Icon name="setting" size="big" color="yellow" />
+      </div>
     </div>
   );
 }
