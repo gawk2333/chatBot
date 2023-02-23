466c4a613a1478f041ec0336a9716e8ac39a23b8
Kang Gao <kang.g1988@gmail.com>
2023-02-24 02:47:33 +1300

    fix mobile vh issue

--- src/app/containers/BodyContainer/BodyContainer.tsx
+++ src/app/containers/BodyContainer/BodyContainer.tsx
@@ -4,9 +4,16 @@ import ContentBox from "../ContentBox";
 import styles from "./BodyContainer.module.css";
 
 export default function BodyContainer() {
+  const getMobileHeight = () => {
+    return window.innerHeight;
+  };
+
   return (
     <>
-      <div className={styles.bodycontainer}>
+      <div
+        className={styles.bodycontainer}
+        style={{ height: getMobileHeight() }}
+      >
         <Header />
         <ContentBox />
       </div>
