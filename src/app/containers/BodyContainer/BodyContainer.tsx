aaa95b994a5f65cd7623ee3d2a21e69dfd99a855
Kang Gao <kang.g1988@gmail.com>
2023-02-24 03:15:30 +1300

    make content always fit when resize

--- src/app/containers/BodyContainer/BodyContainer.tsx
+++ src/app/containers/BodyContainer/BodyContainer.tsx
@@ -1,19 +1,18 @@
-import React from "react";
+import React, { useState, useLayoutEffect } from "react";
 import Header from "../Header";
 import ContentBox from "../ContentBox";
 import styles from "./BodyContainer.module.css";
 
 export default function BodyContainer() {
-  const getMobileHeight = () => {
-    return window.innerHeight;
-  };
-
+  const [height, setHeight] = useState<number>(0);
+  useLayoutEffect(() => {
+    window.addEventListener("resize", () => setHeight(window.innerHeight));
+    return () =>
+      window.removeEventListener("resize", () => setHeight(window.innerHeight));
+  }, []);
   return (
     <>
-      <div
-        className={styles.bodycontainer}
-        style={{ height: getMobileHeight() }}
-      >
+      <div className={styles.bodycontainer} style={{ height: height }}>
         <Header />
         <ContentBox />
       </div>
